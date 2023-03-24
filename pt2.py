c95a277a96665e4e11406d5bad74c517e4ee2efc
victor <victoragboighale@gmail.com>
2023-03-24 04:44:31 +0000

    i  made changes

--- pt2.py
+++
@@ -1,7 +0,0 @@
-from sys import argv
-script, filename = argv
-print("We are going to read the file you created")
-open = open(filename)
-print("Reading file .....")
-print(open.read())
-open.close()
